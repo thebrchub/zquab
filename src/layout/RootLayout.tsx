fabc54764ba009951ffe52bf42e81e5307bd20a2
Srikanth <srikanthraj.brchubllp@gmail.com>
2026-07-30 18:49:01 +0530

    updated rootlayout and homepage

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout() {
       
       <Navbar />
       
-      <main className="flex-1 flex flex-col">
+      <main className="flex-1 flex flex-col min-h-0">
         <Outlet />
       </main>
       
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -113,7 +113,7 @@ export default function HomePage() {
   }
 
   return (
-    <div className="flex w-full h-[100dvh] bg-[var(--background)] overflow-hidden">
+    <div className="flex w-full h-full flex-1 bg-[var(--background)] overflow-hidden">
       
       {/* 📱 / 💻 LEFT SIDEBAR (Inbox / Requests) */}
       <div className={`flex-col h-full bg-[var(--background)] border-r border-[var(--border-color)] transition-all duration-300 ${
